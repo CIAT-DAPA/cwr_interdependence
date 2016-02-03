12b6b23d680b57e39b67ba4111c0b7fd7b697bba
stevensotelo <stevenbetancurt@hotmail.com>
2016-02-03 09:57:52 -0500

    fixed the theme for visualization

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('cwrInterdependenceApp')
   .controller('MainCtrl', function ($scope) {
-     var datafile = 'data/json/interdependence.json';
+     var datafile = 'data/json/interdependece_calories.json';
       var aLittleBit = Math.PI / 100000;
 
       d3.json(datafile, function(data) {
@@ -20,13 +20,13 @@ angular.module('cwrInterdependenceApp')
           now: now,
           animationDuration: 500,
           //margin: 125,
-          margin: 125,
+          margin: 100,
           arcPadding: 0.04,
           layout: {
             alpha: 0,
-            threshold: 5,
-            labelThreshold: 5,
-            //colors: 'cd3d08 ec8f00 6dae29 683f92 b60275 2058a5 00a592 009d3c 378974 ffca00'.split(' ').map(function(c) { return '#' + c; })
+            threshold: 1,
+            labelThreshold: 1,
+            colors: 'cd3d08 ec8f00 6dae29 683f92 b60275 2058a5 00a592 009d3c 378974 ffca00'.split(' ').map(function(c) { return '#' + c; })
           }
         });
 
