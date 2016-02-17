e0995a29d797ac5a7808e8b49585dcaa449ddbff
stevensotelo <stevenbetancurt@hotmail.com>
2016-02-17 10:41:02 -0500

    fixed bold text of the navbar

--- web_app/app/scripts/libs/tools/utils.js
+++ web_app/app/scripts/libs/tools/utils.js
@@ -15,4 +15,5 @@ String.prototype.replaceAll = function(search, replacement) {
 $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
+   $(this).css("font-weight","bold");
 });
