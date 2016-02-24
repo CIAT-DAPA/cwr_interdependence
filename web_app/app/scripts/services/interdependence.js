04aa523d7b8014a932dd502a89a438d1a8a5118f
stevensotelo <stevenbetancurt@hotmail.com>
2016-02-24 09:45:41 -0500

    fixed iss10. changed the value of the treshold for the circos diagram in services/interdependence.js

--- web_app/app/scripts/services/interdependence.js
+++ web_app/app/scripts/services/interdependence.js
@@ -42,8 +42,8 @@ angular.module('cwrInterdependenceApp')
                 precision: precision,
                 layout: {
                     alpha: 0,
-                    threshold: 1,
-                    labelThreshold: 1,
+                    threshold: 0.1,
+                    labelThreshold: 0.1,
                     //colors: 'cd3d08 ec8f00 6dae29 683f92 b60275 2058a5 00a592 009d3c 378974 ffca00'.split(' ').map(function (c) { return '#' + c; })
                     colors: 'FF0000 FF4500 EE4000 CD3700 CD0000 8B0000 A2CD5A 66CD00 458B00 228B22 006400 EED5B7 CDAA7D 8B7355 8B4513 EEC900 00BFFF 1E90FF 1C86EE 104E8B 0000CD FFA500 FF8C00'.split(' ').map(function (c) { return '#' + c; })
                 }
